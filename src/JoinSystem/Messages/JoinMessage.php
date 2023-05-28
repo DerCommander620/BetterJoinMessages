a62a156b94292dc93860c74d6879ef88ce3dde9e
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-05-28 21:19:58 +0200

    Update JoinMessage.php

--- src/JoinSystem/Messages/JoinMessage.php
+++ src/JoinSystem/Messages/JoinMessage.php
@@ -10,11 +10,6 @@ use pocketmine\world\sound\ExplodeSound;
 class JoinMessage implements Listener{
 
     public function onJoin(PlayerJoinEvent $event){
-        if(!$event->hasPlayedBefore()){
-            $config = Main::getInstance()->getConfig();
-            $event->setJoinMessage($event->getPlayer()->getName() . " §aIs a new Player. Welcome " . $event->getPlayer()->getName() . "!");
-            $event->getPlayer()->broadcastSound(ExplodeSound);
-        }else{
             $config = Main::getInstance()->getConfig();
             $event->setJoinMessage($config->get("Join.Prefix") . " §a" . $event->getPlayer()->getDisplayName());
         }
