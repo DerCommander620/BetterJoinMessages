1d5b3d61988f72e41191e62849521b9ddb961e0c
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-05-28 21:18:49 +0200

    Update Main.php

--- src/JoinSystem/Main.php
+++ src/JoinSystem/Main.php
@@ -3,8 +3,10 @@ namespace JoinSystem;
 
 use JoinSystem\Messages\JoinMessage;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
 
 class Main extends PluginBase{
+    use SingletonTrait;
 
     public function onLoad(): void{
         self::setInstance($this);
