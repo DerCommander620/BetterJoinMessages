edef9603e0a621b6168f0671c4fe17a3716b7e26
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-05-28 21:20:34 +0200

    Update JoinMessage.php

--- src/JoinSystem/Messages/JoinMessage.php
+++ src/JoinSystem/Messages/JoinMessage.php
@@ -12,6 +12,5 @@ class JoinMessage implements Listener{
     public function onJoin(PlayerJoinEvent $event){
             $config = Main::getInstance()->getConfig();
             $event->setJoinMessage($config->get("Join.Prefix") . " §a" . $event->getPlayer()->getDisplayName());
-        }
     }
 }
