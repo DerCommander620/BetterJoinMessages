d0e47cf3ade111208ac9884d7cc2ccc74c1f4bc2
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-06-08 16:36:16 +0200

    Update LeaveMessage.php

--- src/JoinSystem/Messages/LeaveMessage.php
+++ src/JoinSystem/Messages/LeaveMessage.php
@@ -7,8 +7,8 @@ use pocketmine\event\player\PlayerQuitEvent;
 
 class LeaveMessage implements Listener{
 
-    public function onLeave(PlayerQuitEvent $event){
+    public function onQuit(PlayerQuitEvent $event){
         $player = $event->getPlayer();
-        $event->setQuitMessage(Main::getInstance()->getConfig()->get("Leave.Prefix") . " §c" . $player->getDisplayName());
+        $event->setQuitMessage(str_replace(["{player}"], [$event->getPlayer()->getName()], $config->get("LeaveMessage")));
     }
 }
